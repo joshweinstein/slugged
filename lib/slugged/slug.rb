6aa2a8efc8fd503ae8cc643c0e9917918fd12433
Josh Weinstein <weinstein.josh@gmail.com>
2013-04-21 17:50:09 -0400

    Rails 4 scopes want callable modifiers.

--- lib/slugged/slug.rb
+++ lib/slugged/slug.rb
@@ -1,19 +1,19 @@
 module Slugged
   class Slug < ActiveRecord::Base
     self.table_name = "slugs"
-  
+
     validates_presence_of :record_id, :slug, :scope
-  
-    scope :ordered,    order('created_at DESC')
-    scope :only_slug,  select(:slug)
+
+    scope :ordered,    -> { order('created_at DESC') }
+    scope :only_slug,  -> { select(:slug) }
     scope :for_record, lambda { |r| where(:record_id => r.id, :scope => Slugged.key_for_scope(r)) }
     scope :for_slug,   lambda { |scope, slug| where(:scope=> scope.to_s, :slug => slug.to_s)}
-  
+
     if methods.include? :pluck
       def self.id_for(scope, slug)
         ordered.for_slug(scope, slug).pluck(:record_id).first
       end
-      
+
       def self.previous_for(record)
         ordered.only_slug.for_record(record).pluck(:slug)
       end
@@ -21,12 +21,12 @@ module Slugged
       def self.id_for(scope, slug)
         ordered.for_slug(scope, slug).first.try(:record_id)
       end
-    
+
       def self.previous_for(record)
         ordered.only_slug.for_record(record).all.map(&:slug)
       end
     end
-    
+
     def self.record_slug(record, slug)
       scope = Slugged.key_for_scope(record)
       # Clear slug history in this scope before recording the new slug
@@ -38,14 +38,14 @@ module Slugged
         history.save
       end
     end
-    
+
     def self.remove_history_for(record)
       for_record(record).delete_all
     end
-  
+
     def self.usable?
       table_exists? rescue false
     end
-  
+
   end
 end
